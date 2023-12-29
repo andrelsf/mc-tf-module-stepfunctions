efb9846adfa69f0de384da00a6c92e1e43d53cce
Andre Ferreira <andre.ferreira@multicode.dev.br>
2023-12-29 19:09:35 -0300

    Fixed state machine name and definition

--- example/main.tf
+++ example/main.tf
@@ -1,8 +1,8 @@
 module "state_machine" {
   source                     = "git::https://github.com/andrelsf/mc-tf-module-stepfunctions.git?ref=1.0.0"
-  state_machine_name         = "asl-definition/state_machine.json"
+  state_machine_name         = "hello-world-state-machine-workflow"
   state_machine_type         = "EXPRESS"
   state_machine_iam_role_arn = "arn:aws:iam::111122223333:role/service-role/SateMachineExecutionRole"
   state_machine_log_level    = "ERROR"
-  state_machine_definition   = templatefile("als-definition/state_machine.json", {})
+  state_machine_definition   = templatefile("asl-definition/state_machine.json", {})
 }
