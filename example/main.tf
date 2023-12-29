ab96ed7a532cac1c4b8824b00ed84db2f41ae825
Andre Ferreira <andre.ferreira@multicode.dev.br>
2023-12-29 19:05:06 -0300

    Updated source and state machine definition

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,6 @@
 module "state_machine" {
-  source                     = "git::"
-  state_machine_name         = "hello-world-state-machine-workflow"
+  source                     = "git::https://github.com/andrelsf/mc-tf-module-stepfunctions.git?ref=1.0.0"
+  state_machine_name         = "asl-definition/state_machine.json"
   state_machine_type         = "EXPRESS"
   state_machine_iam_role_arn = "arn:aws:iam::111122223333:role/service-role/SateMachineExecutionRole"
   state_machine_log_level    = "ERROR"
